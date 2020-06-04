436f08ce7d1184f650b3d191db81cfa595ca4603
Jacob <perezjacob2@gmail.com>
2020-06-04 00:27:22 -0500

    Making progress

---
+++ js/contact.js
@@ -0,0 +1,56 @@
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    validation();
+});
+
+function validation() {
+    var firstName, lastName, email, message ;
+    
+    firstName = document.forms["form"]["firstName"].value;
+    lastName = document.forms["form"]["lastName"].value;
+    message = document.forms["form"]["textarea"].value;
+    email = document.forms["form"]["email"].value;
+    
+    
+    if(firstName== null || firstName== "") {
+         document.getElementById("formStatus").innerHTML  = "Please enter your first name.";
+         form.firstName.focus();
+         return false;
+    }
+
+    if(lastName== null || lastName== "") {
+        document.getElementById("formStatus").innerHTML  = "Please enter your last name.";
+        form.lastName.focus();
+        return false;
+   }
+   
+    if(message== null || message== "") {
+         document.getElementById("formStatus").innerHTML  = "Please enter your message.";
+         form.textarea.focus();
+         return false;
+    }
+    
+    if(email==null || email=="") {
+         document.getElementById("formStatus").innerHTML  = "Please enter your email address.";
+         form.email.focus();
+         return false;
+    }
+    
+    alert("Form Submitted Successfully!")
+    return true;
+    }
+   
+
+
+(function() {
+    
+    var checkLength = function() {
+        var maxText = 150, msgLength;
+         msgLength = document.forms["form"]["textarea"].value;
+         if(msgLength.length <= maxText) {
+              document.getElementById("textLength").innerHTML = "Max " +(maxText - msgLength.length) + " Characters";
+         }
+    }
+    setInterval(checkLength, 300);
+    
+}());
